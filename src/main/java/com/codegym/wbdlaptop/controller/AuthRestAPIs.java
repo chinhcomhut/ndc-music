5f9abd16d3dec10773fa10a18f46faaa2bb97837
chinhcomhut <chinhcomhut@gmail.com>
2020-06-05 12:02:43 +0700

    sua link changeProfile

--- src/main/java/com/codegym/wbdlaptop/controller/AuthRestAPIs.java
+++ src/main/java/com/codegym/wbdlaptop/controller/AuthRestAPIs.java
@@ -156,7 +156,7 @@ public class AuthRestAPIs {
     }
 
 
-    @PutMapping("/update-password/{id}")
+    @PutMapping("/changeProfile/{id}")
     public ResponseEntity<?>updatePassword(@Valid @RequestBody PasswordForm passForm, @PathVariable Long id) {
         Optional<User> user = userService.findById(id);
 
