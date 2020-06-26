e025602d1244bb741130948da856a62d153132cb
chinhcomhut <chinhcomhut@gmail.com>
2020-06-26 14:51:41 +0700

    cau hinh cors

--- src/main/java/com/codegym/wbdlaptop/config/AppConfig.java
+++ src/main/java/com/codegym/wbdlaptop/config/AppConfig.java
@@ -1,10 +1,36 @@
 package com.codegym.wbdlaptop.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
 
 @Configuration
 @EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
-}
+    @Component
+    public class CorsFilter implements Filter {
+
+        public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+            HttpServletResponse response = (HttpServletResponse) res;
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+            chain.doFilter(req, res);
+        }
+
+        @Override
+        public boolean isLoggable(LogRecord record) {
+            return false;
+        }
+    }}
